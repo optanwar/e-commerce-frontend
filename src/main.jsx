d0274cb24188ec3bdbd920841f9942217b0670bc
optanwar <optanwar10@gmail.com>
2025-06-04 18:36:28 +0530

    create prfect folder structure

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import ScrollToTop from "./components/ScrollToTop";
-import './App.css'
-import './Tailwind.css';
+import './styles/App.css'
+import './styles/Tailwind.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
